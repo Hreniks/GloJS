3c53e501fd592ec6afdf2b0a9f5c121e5e164390
hreniks <sbigor09@gmail.com>
2020-10-09 19:35:53 +0300

    первый коммит

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let money = 30000;
+let income = "фриланс";
+let addExpenses = "интернет,такси,коммуналка";
+let deposit = false;
+let mission = 1000000;
+let period = 3;
+let budgetDay = money / 30;
+
+
+
+console.log(typeof money, typeof income, typeof deposit);
+console.log('length: ', addExpenses.length);
+console.log("Период равен " + period + " месяца ",`Цель заработать ${mission} долларов`);
+console.log(addExpenses.toLowerCase().split(','));
+console.log('budgetDay: ', budgetDay);
