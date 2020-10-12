3382af0eb7f3dd5449e41e8d380d088d05d24c2c
hreniks <sbigor09@gmail.com>
2020-10-13 01:14:19 +0300

    lesson03 finished homework

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let addExpenses = "интернет,такси,коммуналка";
 let deposit = false;
 let mission = 1000000;
 let period = 3;
-let budgetDay = money / 30;
+var budgetDay = money/ 30;
 
 
 
@@ -14,6 +14,26 @@ console.log("Период равен " + period + " месяца ",`Цель з�
 console.log(addExpenses.toLowerCase().split(','));
 console.log('budgetDay: ', budgetDay);
 
-mission = prompt("Ваш месячный доход?");
+money = prompt("Ваш месячный доход?", 30000);
 addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую");
 deposit = confirm("Есть ли у вас депозит в банке?");
+
+let exspenses1 = prompt("Введите обязательную статью расходов");
+let exspenses2 = prompt("Введите обязательную статью расходов");
+
+let amount1 = prompt("Во сколько это обойдется?");
+let amount2 = prompt("Во сколько это обойдется?");
+
+var budgetMonth = money - amount1 - amount2;
+console.log("Бюджет на месяц: ", budgetMonth);
+console.log("Цель будет достигнута за: ", Math.ceil(mission / budgetMonth));
+budgetDay = Math.floor(budgetMonth / 30);
+console.log('Бюджет на день: ', budgetDay);
+
+budgetDay > 1200 ? console.log("У вас высокий доход") :
+budgetDay > 600 && budgetDay < 1200 ? console.log("У вас средний уровень дохода") :
+budgetDay < 600 && budgetDay > 0 ? console.log("К сожалению у вас уровень дохода ниже среднего") :
+budgetDay < 0 ? console.log("Что то пошло не так") : 
+budgetDay === 1200 ? console.log("Какой-то доход") : 
+budgetDay === 600 ? console.log("Какой-то доход") :
+budgetDay === 0 ? console.log("Все очень плохо") : console.log('error');
