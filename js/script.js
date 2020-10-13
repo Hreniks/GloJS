52706c2e860619d19590e5c91c5e5723e7340d44
hreniks <sbigor09@gmail.com>
2020-10-13 18:58:05 +0300

    доделать

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-let money = prompt("Ваш месячный доход?", 30000);
-let income = "фриланс";
-let addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую");
-let deposit = confirm("Есть ли у вас депозит в банке?");
+const money = prompt("Ваш месячный доход?", 30000);
+const income = "фриланс";
+const addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую");
+const deposit = confirm("Есть ли у вас депозит в банке?");
 const mission = 1000000;
 const period = 3;
 let budgetDay = money / 30;
@@ -11,10 +11,9 @@ console.log(typeof money, typeof income, typeof deposit);
 console.log('length: ', addExpenses.length);
 console.log("Период равен " + period + " месяца ",`Цель заработать ${mission} долларов`);
 console.log(addExpenses.toLowerCase().split(','));
-console.log('budgetDay: ', budgetDay);
 
-let exspenses1 = prompt("Введите обязательную статью расходов");
-let exspenses2 = prompt("Введите обязательную статью расходов");
+const exspenses1 = prompt("Введите обязательную статью расходов");
+const exspenses2 = prompt("Введите обязательную статью расходов");
 
 const amount1 = prompt("Во сколько это обойдется?");
 const amount2 = prompt("Во сколько это обойдется?");
@@ -30,4 +29,5 @@ console.log('Бюджет на день: ', budgetDay);
 budgetDay >= 1200 ? console.log("У вас высокий доход") :
 budgetDay >= 600 && budgetDay < 1200 ? console.log("У вас средний уровень дохода") :
 budgetDay < 600 && budgetDay >= 0 ? console.log("К сожалению у вас уровень дохода ниже среднего") :
-budgetDay < 0 ? console.log("Что то пошло не так") : console.log("error");
+budgetDay < 0 ? console.log("Что то пошло не так") : 
+console.log("error");
