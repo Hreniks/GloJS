bb16da2b7ffc62f97e2d0564618384f8c179857a
Hreniks <sbigor09@gmail.com>
2020-11-03 23:38:07 +0300

    fixed lesson16second

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 /* Напишите функцию на JS. Цель: Убрать все объекты с типом additional, а для basic очки уменьшить в двое.
-
 Изменить необходимо исходный массив*/
 'use strict';
-const myLesson = [
+let myLesson = [
     {lesson: 1, type: 'basic', points: 2},
     {lesson: 2, type: 'additional', points: 4},
     {lesson: 3, type: 'basic', points: 6},
@@ -22,58 +21,12 @@ const myLesson = [
   ];
   
 
-function newMyLesson(){
 
-
-    return myLesson.filter((item) => {
-                if (item.type !== 'additional'){
-                    return item;
-                }}).map((item) => {
-                    if (item.type === 'basic' && item !== undefined){
-                        item.points = item.points / 2;           
-                        }
-                    return item;
-                });
+function newMyLesson(){  
+  myLesson = myLesson.filter(item => item.type !== 'additional').map(item => ({...item, points: item.points / 2}));
+  console.log(myLesson);
 }
-
-//newMyLesson();
-console.log('newMyLesson(): ', newMyLesson());
-
-
-//   function newArr(...arr){
-//         //console.log(...arr);
-//     let result = arr.filter((item) => {
-//         if (item.type !== 'additional'){
-//             return item;
-//         }
-//     });
-//     //let [lesson,basic,points] = result;
-//         result = result.filter((item) =>{
-//             if (item.type === 'basic'){
-//                 item.points = item.points / 2;
-
-//             }
-//             return item;
-//         });
-//       arr = result.map(item => item);
-//     return arr;
-//   }
   
-  //console.log('newArr(myLesson);: ', newArr(...myLesson));\
 
-
-//     console.log(arr);
-//    let result = myLesson.map((item) => {
-//         if (item.type !== 'additional'){
-//             return item;
-//         }
-//     });
-//     console.log(arr);
-//     result = result.map((item) =>{
-//         if (item.type === 'basic' && item !== undefined){
-//             item.points = item.points / 2;
-            
-//         }
-//          return item;
-//     });
-//      return arr;
+newMyLesson();
+    
