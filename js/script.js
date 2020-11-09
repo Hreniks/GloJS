9a300aa7c9833231a29c029681e48546a572e31a
hreniks <sbigor09@gmail.com>
2020-11-09 04:06:50 +0300

    added .jshinrc

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
+
 'use strict';
 
+class Todo {
+    
+    constructor(form, input, todoList, todoCompleted) {
+        this.form = document.querySelector(form);
+        this.input = document.querySelector(input);
+        this.todoList = document.querySelector(todoList);
+        this.todoCompleted = document.querySelector(todoCompleted);
+
+    }
+
+    init(){
+
+    }
+}
+
+const todo = new Todo('.todo-control', '.header-input', '.todo-list', '.todo-completed');
+
+todo.init();
